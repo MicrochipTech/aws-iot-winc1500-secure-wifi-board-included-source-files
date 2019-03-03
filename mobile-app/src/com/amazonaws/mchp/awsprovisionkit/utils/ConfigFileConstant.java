f3f1b10512273ebc88f84de430494f23defcd217
Ben Poon <Ben.Poon@microchip.com>
2019-03-03 17:12:14 +0800

    - remove the AWS credentials in the mobile app config files - update the mobile app release version - commit the missing files of the mobile APP

--- mobile-app/src/com/amazonaws/mchp/awsprovisionkit/utils/ConfigFileConstant.java
+++ mobile-app/src/com/amazonaws/mchp/awsprovisionkit/utils/ConfigFileConstant.java
@@ -14,14 +14,18 @@ public class ConfigFileConstant {
     public static final String DB_TABLE_NAME_ATTR = "IndexTable";
 
 
-    public static final String CUSTOMER_SPECIFIC_ENDPOINT_DEFAULT_VAL = "a3adakhi3icyv9.iot.us-east-1.amazonaws.com";
-    public static final String COGNITO_POOL_ID_DEFAULT_VAL = "us-east-1:c4f36b6a-84c7-4acd-b143-0665f9fbd9f9";
-    public static final String AWS_IOT_POLICY_NAME_DEFAULT_VAL = "secure-wifi-board-mobile-app-policy";
+    //========================================================================
+    // User need to set below parameters to your AWS account credentials
+    //========================================================================
+
+    public static final String CUSTOMER_SPECIFIC_ENDPOINT_DEFAULT_VAL = "xxxxxxxxxxxxxxxxx.us-east-1.amazonaws.com";
+    public static final String COGNITO_POOL_ID_DEFAULT_VAL = "us-east-1:xxxxxx-xxxxx-xxxx-xxxx-xxxxxxxxxxxxx";
+    public static final String AWS_IOT_POLICY_NAME_DEFAULT_VAL = "AWS_IOT_POLICY_NAME";
     public static final String AWS_IOT_REGION_DEFAULT_VAL = "us-east-1";
-    public static final String COGNITO_USER_POOL_ID_DEFAULT_VAL = "us-east-1_h69ZCLKJr";
+    public static final String COGNITO_USER_POOL_ID_DEFAULT_VAL = "us-east-1_xxxxxxx";
     public static final String COGNITO_REGION_DEFAULT_VAL = "us-east-1";
-    public static final String COGNITO_CLIENT_ID_DEFAULT_VAL = "2l8mk3r49t49ebk8gj5mlad81a";
-    public static final String COGNITO_CLIENT_SECRET_DEFAULT_VAL = "n3637ufueg9kdt2r76bfg36ln156dcfsfaqpehss3jiajrob98g";
+    public static final String COGNITO_CLIENT_ID_DEFAULT_VAL = "xxxxxxxxxxxxxxxxxxxxxxxx";
+    public static final String COGNITO_CLIENT_SECRET_DEFAULT_VAL = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
     public static final String DB_TABLE_NAME_DEFAULT_VAL = "SensorBoardAcctTable";
 
 }
